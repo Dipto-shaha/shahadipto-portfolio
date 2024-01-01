7d92c547afef0350c3c0727c2bd396b11676f31d
Dipto Shaha <diptoshaha2635@gmail.com>
2024-01-02 01:00:20 +0600

    About Section Update

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,19 @@
 import self from "../src/assets/img/selfImage.jpg";
+import { FaCircle } from "react-icons/fa";
 const About = () => {
   return (
     <div className="flex lg:flex-row flex-col ">
       <div className="text-xl mt-10 ml-5">
         <p className="text-2xl font-bold mt-5 text-[#00cf5d]">Hello! I am </p>
         <p className="lg:text-7xl text-5xl font-bold mt-1">Dipto Shaha</p>
-        <p className="text-3xl font-bold my-5 text-[#00cf5d]">
-          Full Stack Developer
-        </p>
+        <div className="flex lg:flex-row flex-col text-2xl font-bold my-5 text-[#00cf5d]">
+          <p className="flex  justify-center items-center">
+            <FaCircle className="mr-1" /> Aspiring Software Engineer{" "}
+          </p>
+          <p className="ml-3 flex justify-center items-center">
+            <FaCircle className="mr-1" /> Full Stack Developer{" "}
+          </p>
+        </div>
         <p></p>A computer science student and enthusiastic programmer with
         strong knowledge of data structures and algorithms. Extensive problem
         solving experience, having solved thousands of problems across multiple
